e9280bd58a86cf1f76c751fdc8cee904fd065b74
KSI5 <krissigebs@gmail.com>
2023-08-12 14:18:11 -0700

    fixed the console.py file

--- console.py
+++ console.py
@@ -10,7 +10,6 @@ from models.amenity import Amenity
 from models.place import Place
 from models import storage
 from models.review import Review
-from models.engine.file_storage import FileStorage
 from datetime import datetime
 
 """
@@ -39,11 +38,6 @@ class HBNBCommand(cmd.Cmd):
         ''' Quit command - Use CTRL+D to exit the program '''
         print("")
         return True
-
-
-    def do_nothing(self, arg):
-        ''' Placeholder method that does nothing '''
-        pass
 
     def emptyline(self):
         ''' Empty line handling - ENTER shouldn't execute anything '''
@@ -159,42 +153,6 @@ class HBNBCommand(cmd.Cmd):
             obj_dict[arg[2]] = arg[3]
         storage.save()
 
-    def do_update2(self, args):
-        ''' Update2 command - Modify attributes of an instance using JSON '''
-        if not args:
-            print("** class name missing **")
-            return
-        my_dict = "{" + args.split("{")[1]
-        arg = shlex.split(args)
-        storage.reload()
-        obj = storage.all()
-
-        if arg[0] not in HBNBCommand.All_class_dict.keys():
-            print("** class doesn't exist **")
-            return
-        if len(arg) == 1:
-            print("** instance id missing **")
-            return
-        try:
-            obj_key = arg[0] + "." + arg[1]
-            obj[obj_key]
-        except KeyError:
-            print("** no instance found **")
-            return
-        if my_dict == "{":
-            print("** attribute name missing **")
-            return
-        my_dict = my_dict.replace("\'", "\"")
-        my_dict = json.loads(my_dict)
-        obj_inst = obj[obj_key]
-        for k in my_dict:
-            if hasattr(obj_inst, k):
-                d_type = type(getattr(obj_inst, k))
-                setattr(obj_inst, k, my_dict[k])
-            else:
-                setattr(obj_inst, k, my_dict[k])
-        storage.save()
-
     def do_count(self, args):
         ''' Count command - Count instances of a specific class '''
         obj = storage.all()
@@ -203,45 +161,6 @@ class HBNBCommand(cmd.Cmd):
             if args in key:
                 cnt += 1
         print(cnt)
-
-    def default(self, args):
-        ''' Default command - Define actions on objects {<>}.all(),
-        {<>}.count(), {<>}.show(), {<>}.destroy(), {<>}.update() '''
-        cmd_dict = {
-            "all": self.do_all,
-            "count": self.do_count,
-            "show": self.do_show,
-            "destroy": self.do_destroy,
-            "update": self.do_update,
-        }
-        arg = args.strip()
-        val = arg.split(".")
-        if len(val) != 2:
-            cmd.Cmd.default(self, arg)
-            return
-        class_name = val[0]
-        command = val[1].split("(")[0]
-        line = ""
-        if command == "update" and val[1].split("(")[1][-2] == "}":
-            inputs = val[1].split("(")[1].split(",", 1)
-            inputs[0] = shlex.split(inputs[0])[0]
-            line = "".join(inputs)[0:-1]
-            line = class_name + " " + line
-            self.do_update2(line.strip())
-            return
-        try:
-            inputs = val[1].split("(")[1].split(",")
-            for num in range(len(inputs)):
-                if num != len(inputs) - 1:
-                    line = line + " " + shlex.split(inputs[num])[0]
-                else:
-                    line = line + " " + shlex.split(inputs[num][0:-1])[0]
-        except IndexError:
-            inputs = ""
-            line = ""
-        line = class_name + line
-        if command in cmd_dict.keys():
-            cmd_dict[command](line.strip())
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
