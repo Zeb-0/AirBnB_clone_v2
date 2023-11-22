06067abb7d76272290b97ce2f5b31400241d30cd
Zeberio <morandezeb@gmail.com>
2023-11-22 23:13:00 +0300

    airBnB updates

--- models/amenity.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" This module defines the Amenity class"""
-from models.base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    """Amenity class that inherits BaseModel"""
-    name = ""
